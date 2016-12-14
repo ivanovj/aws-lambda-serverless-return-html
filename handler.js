1f85dc942752737c69e551e6b8d4a4e08d5aca05
J.Ivanov <mygit@jivanov.com>
2016-12-13 23:05:38 -0500

    code commit

---
+++ handler.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+
+  const response = "<html><header><title>Hello World HTML from AWS Lambda</title></header><body><h1>Hello world!</h1><h2>It is HTML returned from AWS Lambda!</h2></body></html>";
+
+  callback(null, response);
+};
