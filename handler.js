08b55ede649f5ec3a032a25a842703e0d44e0ba1
J.Ivanov <mygit@jivanov.com>
2020-04-11 12:33:06 -0400

    Updated env to NodeJS to 12.x + some cleanup

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.html = (event, context, callback) => {
 
   const response = "<html><header><title>Hello World HTML from AWS Lambda</title></header><body><h1>Hello world!</h1><h2>It is HTML returned from AWS Lambda!</h2></body></html>";
 
